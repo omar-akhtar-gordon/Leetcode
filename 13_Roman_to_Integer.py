c2b2d180612ecfedbf74aa9b31771ee97dcf584e
omar-agordon <omar.agordon@btinternet.com>
2025-04-23 20:27:06 +0100

    Create 13_Roman_to_Integer.py

---
+++ 13_Roman_to_Integer.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+        hashmap={
+            'I':1,
+            'V':5,
+            'X':10,
+            'L':50,
+            'C':100,
+            'D':500,
+            'M':1000,       
+        }
+        digit=0
+        s = s.replace("IV", "IIII").replace("IX", "VIIII").replace("XL", "XXXX").replace("XC", "LXXXX").replace("CD", "CCCC").replace("CM", "DCCCC")
+
+
+        for x in s:
+            digit+=hashmap.get(x)
+
+        return digit
